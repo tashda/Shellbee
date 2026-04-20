4e4616af874c0e3699f9950e86ecda097a7a8d36
tashda <kenneth@skabel.se>
2026-04-20 09:04:28 +0200

    Initial Commit

---
+++ buzzr/buzzrApp.swift
@@ -0,0 +1,17 @@
+//
+//  buzzrApp.swift
+//  buzzr
+//
+//  Created by Kenneth Berg on 20/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct buzzrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
